f3d5a2f77b7efe92549d030b2867564d197c783e
mikey1665 <gamermiek@gmail.com>
2014-02-28 21:27:50 -0600

    Tested

--- src/com/Jacob6816/plugins/WizardBrawl/SignManager.java
+++ src/com/Jacob6816/plugins/WizardBrawl/SignManager.java
@@ -1,7 +1,16 @@
 package com.Jacob6816.plugins.WizardBrawl;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 
 public class SignManager implements Listener {
+	
+    @EventHandler
+    public void onSignChange(SignChangeEvent e) {
+            if (e.getLine(0).equalsIgnoreCase("[Test]")) {
+                    e.setLine(0, "§3[Cdd]");
+            }
+    }
 
 }

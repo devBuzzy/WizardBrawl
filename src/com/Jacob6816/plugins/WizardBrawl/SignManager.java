a2fe342f64f697df8fa5a6f9c419e52d7a7dbe5b
mikey1665 <gamermiek@gmail.com>
2014-03-01 00:05:14 -0600

    Added the joinable signs

--- src/com/Jacob6816/plugins/WizardBrawl/SignManager.java
+++ src/com/Jacob6816/plugins/WizardBrawl/SignManager.java
@@ -1,10 +1,17 @@
 package com.Jacob6816.plugins.WizardBrawl;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.Jacob6816.plugins.WizardBrawl.Arenas.Arena;
 import com.Jacob6816.plugins.WizardBrawl.Arenas.ArenaManager;
 import com.Jacob6816.plugins.WizardBrawl.Misc.Permissions;
 
@@ -21,5 +28,21 @@ public class SignManager implements Listener {
             }
         }
     }
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void clickHandler(PlayerInteractEvent e){
+        if(!(e.getAction()==Action.RIGHT_CLICK_BLOCK || e.getAction()==Action.LEFT_CLICK_BLOCK)) return;
+        Block clickedBlock = e.getClickedBlock(); 
+        if(!(clickedBlock.getType()==Material.SIGN || clickedBlock.getType()==Material.SIGN_POST || clickedBlock.getType()==Material.WALL_SIGN)) return;
+        Sign thisSign = (Sign) clickedBlock.getState();
+        String[] lines = thisSign.getLines();       
+        if(lines[0].equalsIgnoreCase("§5[WizardBrawl]")){
+        	Permissions perms = new Permissions(e.getPlayer());
+        	Arena a = ArenaManager.get().getByName(lines[1]);
+        	if(a != null && perms.CanUseArena()) {
+        		a.addPlayer(e.getPlayer());
+        	}
+        		}	
+        	
+    	}
     
-}
+	}

da8164c9719090a2997bd0d1a5bd49f6b2c5f813
mikey1665 <gamermiek@gmail.com>
2014-02-28 21:23:49 -0600

    Added a SignManager

---
+++ src/com/Jacob6816/plugins/WizardBrawl/SignManager.java
@@ -0,0 +1,7 @@
+package com.Jacob6816.plugins.WizardBrawl;
+
+import org.bukkit.event.Listener;
+
+public class SignManager implements Listener {
+
+}
--- src/com/Jacob6816/plugins/WizardBrawl/WizardBrawl.java
+++ src/com/Jacob6816/plugins/WizardBrawl/WizardBrawl.java
@@ -1,11 +1,14 @@
 package com.Jacob6816.plugins.WizardBrawl;
 
+import me.mike1665.gamemode.easygmSign;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WizardBrawl extends JavaPlugin {
     @Override
     public void onEnable() {
-        // TODO Auto-generated method stub
+    	Bukkit.getServer().getPluginManager().registerEvents(new SignManager(), this);
         super.onEnable();
     }
 }

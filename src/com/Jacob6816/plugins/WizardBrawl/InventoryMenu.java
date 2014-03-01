0a290bf12a592a74d9382283db6e295ad9963af4
mikey1665 <gamermiek@gmail.com>
2014-02-28 22:29:37 -0600

    fffff

--- src/com/Jacob6816/plugins/WizardBrawl/InventoryMenu.java
+++ src/com/Jacob6816/plugins/WizardBrawl/InventoryMenu.java
@@ -7,7 +7,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryMenu implements Listener {
-	public static Inventory myInventory = Bukkit.createInventory(null, 9, "Choose an arena to join!");
+	public static Inventory myInventory = Bukkit.createInventory(null, 9, "Choose an arena to jofin!");
 	static {
 		myInventory.setItem(0, new ItemStack(Material.DIRT, 1));
 		myInventory.setItem(8, new ItemStack(Material.GOLD_BLOCK, 1));
